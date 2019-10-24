d3aea440c85029d1e6d42883e537fec05c44829e
Aida Abdikulova <abdikulova@gmail.com>
2019-10-24 01:30:30 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,10 @@ class App < Sinatra::Base
 
   set :views, Proc.new {File.join(root,"../views/")}
   get '/' do
-
+    erb :team_input
   end
   post '/teams' do
-    
+    erb :team_output
   end
 
 end
